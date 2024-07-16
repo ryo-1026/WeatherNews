41035f239eba5987c07fe7fc66eeaf047f2ee52f
ryo-1026 <ryousuke19881026@gmail.com>
2024-07-16 14:22:01 +0900

    レスポンシブスクロール有り

--- resources/views/weather/getweather.blade.php
+++ resources/views/weather/getweather.blade.php
@@ -15,57 +15,64 @@ use Carbon\Carbon;
 </head>
 
 <body>
-    <h1 class="w-full text-center text-3xl mt-8">{{ $prefecture->name }}の今日・明日の天気</h1>
-    <table class="border border-black border-2 mt-6 mx-2 mx-aut text-center">
-        <tbody>
-            <tr class="py-4 border border-b border-black">
-                <td class="text-base px-4 py-2 border-b border-r border-black bg-slate-200">
-                    日付
-                </td>
-                @foreach ($weatherRecords as $weatherRecord)
-                <td class="px-2 py-2 border-r border-b border-black bg-slate-200">
-                    {{ Carbon::parse($weatherRecord->datetime)->format('n月j日 G時') }}
-                </td>
-                @endforeach
-            </tr>
+    <section class="text-center w-full px-2">
+        <h1 class="w-full text-center text-3xl mt-8">{{ $prefecture->name }}の今日・明日の天気</h1>
+        <div class="overflow-x-scroll ">
+            <table id="large" class="border border-black border-2 mt-6 mx-2 px-2 w-[1200px] mx-auto">
+                <tbody class="w-full">
+                    <tr class="py-4 border border-b border-black">
+                        <td class="text-base px-4 py-2 border-b border-r border-black bg-slate-200">
+                            日付
+                        </td>
+                        @foreach ($weatherRecords as $weatherRecord)
+                        <td class="px-2 py-2 border-r border-b border-black bg-slate-200">
+                            {{ Carbon::parse($weatherRecord->datetime)->format('n/j') }}
+                            <br>
+                            {{ Carbon::parse($weatherRecord->datetime)->format('G時') }}
+                        </td>
+                        @endforeach
+                    </tr>
 
-            <tr class="py-4">
-                <td class="text-base py-2 border-b border-r border-black">
-                    天気
-                </td>
-                @foreach ($weatherRecords as $weatherRecord)
-                <td class="px-2 py-2 border-r border-b border-black">
-                    {{ $weatherRecord->description }}
-                </td>
-                @endforeach
-            </tr>
+                    <tr class="py-4">
+                        <td class="text-base py-2 border-b border-r border-black bg-slate-200">
+                            天気
+                        </td>
+                        @foreach ($weatherRecords as $weatherRecord)
+                        <td class="px-2 py-2 border-r border-b border-black">
+                            {{ $weatherRecord->description }}
+                        </td>
+                        @endforeach
+                    </tr>
 
-            <tr class="py-4">
-                <td class="text-base py-2 border-b border-r border-black">
-                    気温
-                </td>
-                @foreach ($weatherRecords as $weatherRecord)
-                <td class="px-2 py-2 border-r border-b border-black">
-                    {{ $weatherRecord->temperature }} ℃
-                </td>
-                @endforeach
-            </tr>
+                    <tr class="py-4">
+                        <td class="text-base py-2 border-b border-r border-black bg-slate-200">
+                            気温
+                        </td>
+                        @foreach ($weatherRecords as $weatherRecord)
+                        <td class="px-2 py-2 border-r border-b border-black">
+                            {{ $weatherRecord->temperature }} ℃
+                        </td>
+                        @endforeach
+                    </tr>
 
-            <tr class="py-4">
-                <td class="text-base py-2 border-b border-r border-black">
-                    降水<br>確率
-                </td>
-                @foreach ($weatherRecords as $weatherRecord)
-                <td class=" px-2 py-2 border-r border-b border-black">
-                    {{ $weatherRecord->precipitation_probability }} %
-                </td>
-                @endforeach
-            </tr>
-        </tbody>
-    </table>
-    <div class="text-center mt-10">
-        <a class="bg-neutral-200 border rounded-md border-black px-8 py-2 hover:bg-neutral-400 " href="{{ url('/') }}">戻る</a>
-    </div>
+                    <tr class="py-4">
+                        <td class="text-base py-2 border-b border-r border-black bg-slate-200">
+                            降水<br>確率
+                        </td>
+                        @foreach ($weatherRecords as $weatherRecord)
+                        <td class="px-2 py-2 border-r border-b border-black">
+                            {{ $weatherRecord->precipitation_probability }} %
+                        </td>
+                        @endforeach
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <div class="text-center mt-10">
+            <a class="bg-neutral-200 border rounded-md border-black px-8 py-2 hover:bg-neutral-400 " href="{{ url('/') }}">戻る</a>
+        </div>
+    </section>
 
 </body>
 
--- resources/views/weather/index.blade.php
+++ resources/views/weather/index.blade.php
@@ -4,28 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>地域別天気予報</title>
+    <title>天気予報</title>
     @vite('resources/css/app.css')
 </head>
 
 <body>
     <section class="p-10">
         <h1 class="w-full text-center text-3xl mt-8">都道府県別天気情報</h1>
-        <div class="mx-auto border border-black border-2 p-8 mt-10 w-1/2">
+        <div class="mx-auto border rounded-md border-black border-2 p-8 mt-10 lg:w-1/2 md:w-3/4">
             @if (session('error'))
             <p class="text-red-600">{{ session('error') }}</p>
             @endif
             <form class="text-center" action="{{ route('weather.getweather') }}" method="POST">
                 @csrf
-                <div class="mb-4">
-                    <label for="prefecture">都道府県を選択してください　:　</label>
-                    <select id="prefecture" class="border border-black" name="prefecture_id" required>
-                        @foreach ($prefectures as $prefecture)
-                        <option value="{{ $prefecture->id }}">{{ $prefecture->name }}</option>
-                        @endforeach
-                    </select>
+                <div class="mb-4 text-center">
+                    <label for="prefecture w-full block">都道府県を選択してください</label>
+                    <div class="w-full mt-4">
+                        <select id="prefecture" class="border border-black" name="prefecture_id" required>
+                            @foreach ($prefectures as $prefecture)
+                            <option value="{{ $prefecture->id }}">{{ $prefecture->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
                 </div>
-                <button class="w-1/2 mt-10 bg-neutral-200 border rounded-md border-black px-8 py-2 hover:bg-neutral-400  block mx-auto" type="submit">天気情報を取得</button>
+                <button class="lg:w-1/2 md:3/4 mt-10 bg-neutral-200 border rounded-md border-black px-8 py-2 hover:bg-neutral-400  block mx-auto" type="submit">天気情報を取得</button>
             </form>
         </div>
 
